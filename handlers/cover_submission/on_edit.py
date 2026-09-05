e288e87a8fe8a05e0296234f0bbfb55ec3c63015
Ethan <205045204+ethancatmew@users.noreply.github.com>
2026-09-05 00:19:35 -0500

    moved edit code from old file to this

---
+++ handlers/cover_submission/on_edit.py
@@ -0,0 +1,25 @@
+import discord
+from discord import ui
+from discord.ext import commands
+
+class EditModal(ui.Modal, title = "Edit Submission"):
+    def __init__(self, bot: commands.Bot, source: str):
+        super().__init__()
+        self.bot = bot
+
+        top, code = source.split("\n", 1)
+        self.top = top
+        self.code = code
+
+    edit = ui.TextInput(
+        label = "Module Code",
+        required = True,
+        style = discord.TextStyle.paragraph
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(content = f"{self.top}\n{self.edit.value}")
+
+async def edit(bot: commands.Bot, interaction: discord.Interaction):
+    source = interaction.message.content
+    await interaction.response.send_modal(EditModal(bot, source))
